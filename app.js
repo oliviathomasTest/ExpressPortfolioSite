3e9c6aa2c8e05324d9be12d43b97801fd6a573ee
Olivia Thomas <oliviathomas637@gmail.com>
2021-02-03 12:36:55 +0530

    FirstCommit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  compress = require("compression"),
+  session = require("express-session"),
+  config = require("./config/env/development"),
+  indexRoutes = require("./routes/index");
+
+const app = express();
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else if (process.env.NODE_ENV === "production") {
+  app.use(compress());
+}
+
+app.use(express.static("./public"));
+
+app.use(
+  session({
+    saveUninitialized: true,
+    secret: config.sessionSecret,
+    resave: true,
+  })
+);
+
+app.set("view engine", "ejs");
+
+app.use(indexRoutes);
+
+app.listen(port, () =>
+  console.log(`Express portfolio app listening on port ${port}!`)
+);
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.post("/", (req, res) => {
+  const firstname = req.body.ifirstname;
+  const lastname = req.body.ilastname;
+
+  const email = req.body.iemail;
+  const message = req.body.imessage;
+
+  console.log(firstname, lastname, email, message);
+
+  res.redirect("/submit");
+});
+
+router.get("/submit", (req, res) => {
+  res.render("submit");
+  console.log("reached submit");
+});
+
+router.post("/submit", (req, res) => {
+  console.log("back to home");
+  res.redirect("/");
+});
+
+router.get("/projects", (req, res) => {
+  res.render("projects");
+});
+
+router.get("/services", (req, res) => {
+  res.render("services");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+module.exports = router;
