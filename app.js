04c6e960b7a48e27653a1885b9e0ea3f1aad2322
Olivia Thomas <oliviathomas637@gmail.com>
2021-02-09 14:13:16 +0530

    Inline Commenting

--- app.js
+++ app.js
@@ -35,10 +35,13 @@ app.use(
     resave: true,
   })
 );
+
 // set the view engine to ejs
 app.set("view engine", "ejs");
+
 // Registering the index routes 
 app.use(indexRoutes);
+
 //Rerouting in case of non-existent routes
 app.use((req, res, next) => {
   res.status(404).redirect('/error');
